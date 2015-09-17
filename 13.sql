a72f6d5c0961623a458f5c53e092e1384dbc07a8
rittycheriah <rittycheriah@gmail.com>
2015-09-16 23:29:15 -0500

    13 & 19 completed

--- 13.sql
+++ 13.sql
@@ -1,3 +1,9 @@
-SELECT trck.Name, trck.TrackId AS trxid, invL.TrackId AS invtrckid FROM InvoiceLine invL JOIN Track trck WHERE trxid=invtrckid;
+SELECT DISTINCT invL.InvoiceLineId, trx.Name, art.Name, trx.TrackId as trxId, invL.TrackId AS invTID, art.ArtistId AS AArtistId
+FROM InvoiceLine invL 
+JOIN Track trx 
+ON trxId=invTID
+LEFT JOIN Album alb
+ON trxId=alb.ArtistId
+LEFT JOIN Artist art
+ON AArtistId=alb.ArtistId;
 
-incomplete solution
