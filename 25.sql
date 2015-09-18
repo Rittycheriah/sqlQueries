615c112d711004985e0a6a5b0749c9c8500eea77
rittycheriah <rittycheriah@gmail.com>
2015-09-17 22:53:10 -0500

    25 refactored

--- 25.sql
+++ 25.sql
@@ -1,5 +1,7 @@
-SELECT COUNT(InvoiceLine.TrackId) AS TotalBought, InvoiceLine.TrackId
+SELECT COUNT(InvoiceLine.TrackId) AS TotalBought, InvoiceLine.TrackId AS invTrx 
 FROM InvoiceLine
+JOIN PlaylistTrack plyTrck
+ON invTrx=plyTrck.TrackId
 GROUP BY InvoiceLine.TrackId
 ORDER BY TotalBought DESC
 LIMIT 5;
